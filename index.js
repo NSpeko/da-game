4ac08fa198f0d0df6695277e4193913c5ebdca40
elostryy <elostryy@gmail.com>
2018-06-17 16:59:28 +0300

    local beautify

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + `/src`));
 
 app.get('/game', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/views/index.html'));
+  res.sendFile(path.join(__dirname + `/src/views/index.html`));
 });
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/views/about.html'));
+  res.sendFile(path.join(__dirname + `/src/views/about.html`));
 });
 
-app.listen(3000, function () {
-  console.log('Server running at http://localhost:3000/');
+app.listen(port, function () {
+  console.log(`Server running at http://localhost:${port}/`);
 });
